6ec045c9db615aa3ed779da446c313e3510dae5f
Satwik Rudra <rudrasatwik19@gmail.com>
2022-02-15 10:14:36 -0600

    changes

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>My Details</title>
+  </head>
+  <body>
+      <div class="text-center p-5">
+      <h3><b>"Simple Static Website Assignment Spring 2022 using Cloud Managed Web Service"</b></h3>
+        <h5>Name: Mahipal Reddy Gumireddy</h5>
+        <h5>Student ID: 16321580</h5>
+        <h5>Cloud Provider: Heroku</h5>
+
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
